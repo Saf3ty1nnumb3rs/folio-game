10e2d3f5153f54694308777fdc0c79bbb925bcaf
Josh Sample <joshuawsample@gmail.com>
2018-02-05 18:22:38 -0500

    Added game logic and created buttons

---
+++ README.md
@@ -0,0 +1,12 @@
+Hi! Welcome to my WDI-14 project 1.
+
+We are creating an interpretation of HANG MAN (or in the modern vernacular - HANG PERSON). I am a Star Wars nerd so I will be putting a slight SW twist on things.
+
+The project can be found at BitBalloon: http://kind-austin-8ef5a9.bitballoon.com/ . Enjoy!
+
+Check out my wireframe on Figma https://www.figma.com/file/bgrcuA7km6GNa94tVvhkYAlc/Mandalorian-Hang-Suite.
+
+Feeling nosy? Check project workflow at: https://trello.com/b/38FTc3Z8/wdi-project-1-mandalorian-hang-suite .
+
+For all source code and related documents go to GitHub: https://github.com/Saf3ty1nnumb3rs/folio-game .
+
--- js/app.js
+++ js/app.js
@@ -1,1 +1,83 @@
 $('body').append(`<h1>Hello, Cruel World!</h1>`)
+
+
+
+
+
+
+
+
+
+
+
+
+const createHangman = {
+    puzzleWordRando: function(){
+        createHangman.puzzleWord.push(words[Math.floor(Math.random()*createHangman.words.length)])
+    },
+
+
+alphabet: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q','R','S','T','U','V','W','X','Y','Z'],
+words:['MANDALORIAN', 'NABOO', 'SKYWALKER', 'VADER', 'TATOOINE', 'PALPATINE', 'CLONE', 'GALAXY', 'REBELLION', 'EMPIRE', 'DESTINY', 'ALLIANCE', 'JEDI', 'CANTINA', 'BLASTER', 'MILLENIUM', 'DISTURBANCE', 'DARKNESS', 'BATTLE', 'DEATHSTAR', 'NERFHERDER', 'DROIDS', 'IMPERIAL','CLONES'],
+puzzleWord: [],
+gameOver: false,
+incorrect: 0,
+correct: 0,
+
+pickLetter: function(){
+    checkPuzzleArray();
+
+
+
+
+},
+
+
+
+
+
+
+
+
+
+
+
+
+ }; // letterSelect end section 
+
+const viewBuilder = {
+
+//Builds the letter buttons in the display - defines id and class.
+letterBuild: function (){
+    let alphaArray = createHangman.alphabet;
+    
+    alphaArray.forEach((letter, i)=>{
+        let alphaIndex = 'alpha' + [i];
+        $('body').append(`<button id="letters" class="alpha ${alphaIndex}">${alphaArray[i]}</button>`);
+    })
+},
+puzzleWordBuild: function(){}
+
+
+
+};
+
+const hangController = {
+
+checkLetter: function(){
+
+}
+
+
+
+
+
+};
+
+
+window.onload = function (){
+viewBuilder.letterBuild();
+$('.alpha').on('click', hangController.checkLetter);
+
+
+};
